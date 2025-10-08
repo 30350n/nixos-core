92360630a2c948f07afb2108820af264cb57a241
Bobbe <maxschlecht@web.de>
2025-10-08 13:42:34 +0200

    Add nixos-core.allowUnfree option

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,24 @@
-{lib, ...}: {
+{
+    config,
+    lib,
+    ...
+}: {
     imports = (
         lib.filter
         (file: lib.strings.hasSuffix ".nix" file && file != ./default.nix)
         (lib.filesystem.listFilesRecursive ./.)
     );
 
-    config.nixpkgs.overlays = [(import ../packages)];
+    options.nixos-core = {
+        allowUnfree = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+            default = [];
+        };
+    };
+
+    config = {
+        nixpkgs.overlays = [(import ../packages)];
+        nixpkgs.config.allowUnfreePredicate = package:
+            builtins.elem (lib.getName package) config.nixos-core.allowUnfree;
+    };
 }
