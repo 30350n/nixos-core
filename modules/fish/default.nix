460a399fa1dc7ee416a33a64d11f5888c6daa486
Bobbe <maxschlecht@web.de>
2025-10-18 15:15:24 +0200

    Fix fish loginShell init script

--- modules/fish/default.nix
+++ modules/fish/default.nix
@@ -54,20 +54,15 @@
 
             environment.systemPackages = [pkgs.fishPlugins.tide];
             environment.variables.SHELL = "${config.programs.fish.package}/bin/fish";
-
-            #xdg.desktopEntries.fish = {
-            #    name = "fish";
-            #    noDisplay = true;
-            #};
         }
         (lib.mkIf config.nixos-core.fish.loginShell.enable {
             programs.bash.interactiveShellInit = let
-                extraSkipParents =
+                skipParents =
                     lib.strings.concatMapStringsSep " && " (cmd: "$_parent != \"${cmd}\"")
-                    config.nixos-core.fish.loginShell.skipParents;
+                    (["fish"] ++ config.nixos-core.fish.loginShell.skipParents);
             in ''
                 _parent=$(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm)
-                if [[ $_parent != "fish" && ${extraSkipParents} && -z "$BASH_EXECUTION_STRING" ]]
+                if [[ ${skipParents} && -z "$BASH_EXECUTION_STRING" ]]
                 then
                     unset _parent
                     shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
