743c0809cae72add25993fb9a1ad0ff77a556777
Bobbe <maxschlecht@web.de>
2025-12-03 12:49:26 +0100

    Fix lib.nixos-core.autoImport error for directories missing default.nix

--- lib.nix
+++ lib.nix
@@ -1,10 +1,10 @@
 lib:
 lib.extend (final: prev: {
-    nixos-core.autoImport = path: (final.mapAttrsToList (
+    nixos-core.autoImport = path: (builtins.filter final.pathExists (final.mapAttrsToList (
         file: type:
             if type == "directory"
             then path + "/${file}/default.nix"
             else path + "/${file}"
     )
-    (final.filterAttrs (file: _: file != "default.nix") (builtins.readDir path)));
+    (final.filterAttrs (file: _: file != "default.nix") (builtins.readDir path))));
 })
