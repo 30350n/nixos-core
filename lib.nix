c71c713fba1230a691f0a637e7ec327dc57cd146
Bobbe <maxschlecht@web.de>
2026-04-27 19:41:59 +0200

    Fix lib.nixos-core.autoImport to only import .nix files

--- lib.nix
+++ lib.nix
@@ -1,12 +1,14 @@
 lib:
 lib.extend (final: prev: {
     nixos-core.autoImport = directory: (
-        builtins.filter final.pathExists (final.mapAttrsToList (
-            file: type:
-                if type == "directory"
-                then directory + "/${file}/default.nix"
-                else directory + "/${file}"
+        builtins.filter (file: final.pathExists file && lib.hasSuffix ".nix" file) (
+            final.mapAttrsToList (
+                file: type:
+                    if type == "directory"
+                    then directory + "/${file}/default.nix"
+                    else directory + "/${file}"
+            )
+            (final.filterAttrs (file: _: file != "default.nix") (builtins.readDir directory))
         )
-        (final.filterAttrs (file: _: file != "default.nix") (builtins.readDir directory)))
     );
 })
