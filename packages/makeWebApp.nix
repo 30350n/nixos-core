37ca23c2b4ac47e9c4facbfa2a197e5adf5f89f6
Bobbe <maxschlecht@web.de>
2026-05-06 17:09:26 +0200

    Refactor makeWebApp to support preRun, extraArgs, fix icons, profiles

--- packages/makeWebApp.nix
+++ packages/makeWebApp.nix
@@ -1,27 +1,52 @@
 {
     chromium,
     inkscape,
+    lib,
     makeDesktopItem,
     runCommand,
+    symlinkJoin,
+    writeShellScriptBin,
 }: {
     url,
     name,
+    app_id ? lib.toLower (builtins.replaceStrings [" "] ["_"] name),
     icon ? null,
     icon-scale ? null,
+    preRun ? "",
+    extraArgs ? "",
+    comment ? null,
+    categories ? [],
 }:
-makeDesktopItem {
-    exec = "${chromium}/bin/chromium --app=http://${url}";
-    desktopName = name;
-    name = let
-        base_url = builtins.elemAt (builtins.match "([^:/]+://)?([^:/]+)(:[0-9]+)?(/.*)?" url) 1;
-    in "chrome-${base_url}__-Default";
-    icon =
-        if icon == null || icon-scale == null
-        then icon
-        else
-            runCommand (baseNameOf icon) {} ''
-                ${inkscape}/bin/inkscape "${icon}" \
-                    --actions "select-all;transform-scale:${toString icon-scale}; \
-                        export-filename:$out;export-do"
-            '';
+symlinkJoin rec {
+    inherit name;
+    paths = let
+        script = writeShellScriptBin app_id ''
+            ${preRun}
+            ${chromium}/bin/chromium \
+                --app=http://${url} \
+                --user-data-dir="$XDG_CONFIG_HOME/pwa-${app_id}" \
+                --profile-directory=${app_id} \
+                ${extraArgs}
+        '';
+    in [
+        script
+        (makeDesktopItem {
+            exec = "${script}/bin/${app_id}";
+            desktopName = name;
+            name = let
+                base_url =
+                    builtins.elemAt (builtins.match "([^:/]+://)?([^:/]+)(:[0-9]+)?(/.*)?" url) 1;
+            in "chrome-${base_url}__-${app_id}";
+            icon =
+                if icon == null || icon-scale == null
+                then icon
+                else
+                    runCommand (baseNameOf icon) {} ''
+                        ${inkscape}/bin/inkscape "${icon}" \
+                            --actions "select-all;transform-scale:${toString icon-scale}; \
+                                export-filename:$out;export-do"
+                    '';
+            inherit comment categories;
+        })
+    ];
 }
